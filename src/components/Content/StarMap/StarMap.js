91b90f2a56ead00cdd6e1ec61bcdb58031cca7f9
TokArsi <ares.r.023@gmail.com>
2023-06-02 14:59:09 +0300

    second

--- src/components/Content/StarMap/StarMap.js
+++ src/components/Content/StarMap/StarMap.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './starmap.scss'
+import ConstellationBrandDevelopment
+    from "../../Constellations/ConstellationBrandDevelopment/ConstellationBrandDevelopment";
 
 const StarMap = () => {
     return (
         <div className="star-map-wrapper">
             <div className="star-map-content">
-
+                <ConstellationBrandDevelopment/>
             </div>
         </div>
     )
