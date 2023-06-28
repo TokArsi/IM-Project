84622713b19b63d43a20f3c14aa1f1386b9861b3
TokArsi <ares.r.023@gmail.com>
2023-06-28 14:34:04 +0300

    51

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -86,7 +86,10 @@ export const AuthorPrivacyComponent = () => {
       <div className="author-privacy-wrapper">
           <div className="authors">
               <div className="developers">
-                  Разработано: Анастасия Тукмакова, Нурия Ситдикова, Токтогонов Арсен, Исмайлова Алина
+                  Разработано: Анастасия Тукмакова, Нурия Ситдикова
+              </div>
+              <div className="developers">
+                  Реализовано: Токтогонов Арсен, Исмайлова Алина
               </div>
               <div className="designers">
                   Оформлено: Светлана Кулакова
